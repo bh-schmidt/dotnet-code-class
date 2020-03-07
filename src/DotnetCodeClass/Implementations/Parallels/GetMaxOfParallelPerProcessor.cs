e53b3b3ef75eb25d3a64b518043048a0ee5d852a
Bruno Hammerschmidt <re4deto@hotmail.com>
2020-03-07 09:30:14 -0300

    Get max of parallelism added.

---
+++ src/DotnetCodeClass/Implementations/Parallels/GetMaxOfParallelPerProcessor.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetCodeClass.Implementations.Parallels
+{
+    public class GetMaxOfParallelPerProcessor : BaseTests
+    {
+        const long times = 1000;
+        [Test]
+        public void GetMaxOfParallelism()
+        {
+            long maxOfParallelism = 0;
+            Parallel.For(0, times, index =>
+            {
+                if (maxOfParallelism > 0)
+                {
+                    return;
+                }
+
+                Thread.Sleep(FirstWait());
+                maxOfParallelism++;
+            });
+
+        }
+
+        private TimeSpan FirstWait()
+        {
+            return TimeSpan.FromSeconds(5);
+        }
+    }
+}
